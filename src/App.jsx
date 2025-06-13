3bff0ca6e5183b8ee18edef1c43baa313b8d2577
Pratiksha-Patil01 <pratikshapatil0108@gmail.com>
2025-06-13 12:55:31 +0530

    secondfour commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           show={showPopup}
           onClose={handleClosePopup}
           studentName="Pratiksha Vijay Patil"
-          studentPhotoUrl="/images/patil.jpeg" // Path to their photo
+          studentPhotoUrl="\public\images\partu.jpeg" // Path to their photo
           uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
         />
       </div>
